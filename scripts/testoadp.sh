a326d815b16f12024fd83b6aa71bfe7eaf2d5082
Sonia Garudi <sonia.garudi1@ibm.com>
2023-02-01 18:34:13 +0530

    Basic test oadp 8

--- scripts/testoadp.sh
+++ scripts/testoadp.sh
@@ -3,6 +3,9 @@
 echo ${KUB_SERVER_URL}
 echo ${KUB_TOKEN}
 cd ${WORKSPACE}
+wget https://mirror.openshift.com/pub/openshift-v4/ppc64le/clients/ocp-dev-preview/latest-4.12/openshift-client-linux-amd64.tar.gz
+tar -xvzf openshift-client-linux-amd64.tar.gz
+
 oc login --token=${KUB_TOKEN} --server=${KUB_SERVER_URL}
 oc get nodes
 
