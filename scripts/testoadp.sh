3f19d99f28e264432e3fd8c2e0d47e771215cd20
Sonia Garudi <sonia.garudi1@ibm.com>
2023-02-01 17:57:06 +0530

    Basic test oadp 2

--- scripts/testoadp.sh
+++ scripts/testoadp.sh
@@ -1,5 +1,10 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo ${KUB_SERVER_URL}
+echo ${KUB_TOKEN}
 cd ${WORKSPACE}
+oc login --token=${KUB_TOKEN} --server=${KUB_SERVER_URL}
+oc get nodes
+
+
 git clone https://github.ibm.com/Sonia-Garudi1/oadp-qe-automation
