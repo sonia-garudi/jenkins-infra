fd6018fc421b1e8585c44c5f3cb4d4a69f77f7ae
Sonia Garudi <sonia.garudi1@ibm.com>
2023-02-01 18:53:12 +0530

    Basic test oadp 12

--- scripts/testoadp.sh
+++ scripts/testoadp.sh
@@ -13,8 +13,9 @@ export PATH=$PATH:${WORKSPACE}
 echo "${BASTION_IP} api.${KUB_SERVER_URL} oauth-openshift.apps.${KUB_SERVER_URL}" >> /etc/hosts
 
 
-oc login --token=${KUB_TOKEN} --server=https://api.${KUB_SERVER_URL}:6443
+oc login --token=${KUB_TOKEN} --server=https://api.${KUB_SERVER_URL}:6443 --insecure-skip-tls-verify
 oc get nodes
 
 
+rm -rf oadp-qe-automation
 git clone https://Sonia-Garudi1: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@github.ibm.com/Sonia-Garudi1/oadp-qe-automation
