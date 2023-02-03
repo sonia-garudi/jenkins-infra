7c9a7d57b61811a89b3b8341e128c1bf638ebadb
Sonia Garudi <sonia.garudi1@ibm.com>
2023-02-03 13:17:22 +0530

    Basic test oadp 20

--- scripts/testoadp.sh
+++ scripts/testoadp.sh
@@ -3,7 +3,7 @@
 cd ${WORKSPACE}
 echo "${BASTION_IP} api.${KUB_SERVER_URL} oauth-openshift.apps.${KUB_SERVER_URL}" >> /etc/hosts
 
-#Install golang
+echo 'Install golang'
 git clone https://github.com/ocp-power-automation/ocp4-playbooks-extras
 cd ocp4-playbooks-extras
 cp examples/go_lang_installation_vars.yaml go_lang_installation_vars.yaml 
@@ -11,7 +11,17 @@ sed -i "s|golang_tarball:.*$|golang_tarball: https://dl.google.com/go/go1.18.lin
 sed -i "s|golang_installation:.*$|golang_installation: true|g" go_lang_installation_vars.yaml
 cp examples/inventory ./e2e_inventory
 ansible-playbook  -i e2e_inventory -e @go_lang_installation_vars.yaml playbooks/golang-installation.yml
+rc=$?
+if [ $rc -ne 0 ] ; then
+  echo 'Golang installation failed'
+  exit
+fi
+export GOROOT=/usr/local/go
+export PATH=/usr/local/go/bin:$PATH
+export GOBIN=/usr/local/go/bin
 
+
+echo 'Login to OCP cluster'
 cd ${WORKSPACE}
 wget https://mirror.openshift.com/pub/openshift-v4/ppc64le/clients/ocp-dev-preview/latest-4.12/openshift-client-linux-amd64.tar.gz
 tar -xvzf openshift-client-linux-amd64.tar.gz
@@ -19,23 +29,33 @@ export PATH=$PATH:${WORKSPACE}
 oc login --token=${KUB_TOKEN} --server=https://api.${KUB_SERVER_URL}:6443 --insecure-skip-tls-verify
 oc get nodes
 
-#Install OADP operator
-echo 'Clone oadp-qe-automation repository'
+
+source ${INSTALL_OPTS_FILE}
 cd ${WORKSPACE} && rm -rf oadp-qe-automation
-export GOROOT=/usr/local/go
-export PATH=/usr/local/go/bin:$PATH
-export GOBIN=/usr/local/go/bin
-git clone https://Sonia-Garudi1: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@github.ibm.com/Sonia-Garudi1/oadp-qe-automation
-#git clone https://${GIT_USERNAME}:${GIT_TOKEN}@github.ibm.com/Sonia-Garudi1/oadp-qe-automation
-cd oadp-qe-automation
-export REPOSITORY='prestage'
-export IP_APPROVAL='Automatic'
-export STREAM='downstream'
-export OADP_VERSION="1.1.2"
-export IIB_IMAGE='iib:422341'
-bash operator/oadp/deploy_oadp.sh 2>&1 | tee deploy-oadp.login
-rc=$?
-if [ $rc -ne 0 ] ; then
-  echo 'Operator installation failed'
+git clone https://${GIT_USERNAME}:${GIT_TOKEN}@github.ibm.com/Sonia-Garudi1/oadp-qe-automation
+	
+#Install OADP operator
+if [ $INSTALL_OADP -eq true ] ; then
+	echo 'Install OADP operator'	
+	cd ${WORKSPACE} && cd oadp-qe-automation
+	bash operator/oadp/deploy_oadp.sh 2>&1 | tee deploy-oadp.log
+	rc=$?
+	if [ $rc -ne 0 ] ; then
+	  echo 'OADP operator installation failed'
+	fi
+	oc get csv -n openshift-adp
 fi
-oc get csv -n openshift-adp
+
+if [ $INSTALL_VOLSYNC -eq true ] ; then
+	echo 'Install Volsync operator'
+	cd ${WORKSPACE} && cd oadp-qe-automation
+	bash operator/volsync/deploy_volsync.sh 2>&1 | tee deploy-volsync.log
+	rc=$?
+	if [ $rc -ne 0 ] ; then
+	  echo 'Volsync operator installation failed'
+	fi
+	oc get csv -n openshift-adp
+fi
+
+#Run e2e
+echo 'Run E2E'
